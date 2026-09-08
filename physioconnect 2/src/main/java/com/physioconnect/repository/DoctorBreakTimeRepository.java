cf0882452f186b7d695732d9b15906ffe44c1d82
DayaNidhi-tech <dsharman111@gmail.com>
2026-09-09 02:07:10 +0530

    Extend doctor break repository

--- physioconnect 2/src/main/java/com/physioconnect/repository/DoctorBreakTimeRepository.java
+++ physioconnect 2/src/main/java/com/physioconnect/repository/DoctorBreakTimeRepository.java
@@ -3,5 +3,9 @@ package com.physioconnect.repository;
 import com.physioconnect.entity.DoctorBreakTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DoctorBreakTimeRepository extends JpaRepository<DoctorBreakTime, Long> {
+    List<DoctorBreakTime> findByDoctorId(Long doctorId);
+    void deleteByDoctorId(Long doctorId);
 }
